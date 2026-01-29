3bdb142aa5374dd52cc3f7449d585bb859118c31
pearseso-pop <pearseso@vcu.edu>
2026-01-29 11:45:24 -0500

    deleting trend line

--- data_visual.R
+++ data_visual.R
@@ -3,7 +3,6 @@ library(ggplot2)
 
 #create visual for mtcars
 ggplot(mtcars, aes(x = cyl, y = mpg)) +
-  geom_point(size = 3) +
   geom_smooth(method = "lm", se = FALSE) +
   labs(
     title = "Relationship between MPG and Cylinders",
