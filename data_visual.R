a8de0fef43e6415eeaec883728bede528ef52954
pearseso-pop <pearseso@vcu.edu>
2026-01-29 11:42:54 -0500

    changed color to purple

--- data_visual.R
+++ data_visual.R
@@ -3,7 +3,7 @@ library(ggplot2)
 
 #create visual for mtcars
 ggplot(mtcars, aes(x = factor(cyl), y = mpg)) +
-  geom_boxplot(fill = "skyblue") +
+  geom_boxplot(fill = "purple") +
   geom_jitter(width = 0.1, alpha = 0.6) +
   labs(
     title = "MPG by Number of Cylinders",
